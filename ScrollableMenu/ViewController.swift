4bfc9119344406fac490dcddcab01af165b4cefa
Saleh Masum <salehmasum@gmail.com>
2022-09-05 12:34:16 +1000

    Initial Commit

---
+++ ScrollableMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollableMenu
+//
+//  Created by Saleh Masum on 5/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
